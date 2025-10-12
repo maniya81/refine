c90fd0749961ec133b6b13af4a63429a9fa77356
Sagar Maniya <maniyasagar26@gmail.com>
2025-10-12 15:37:17 +1100

    feat(deploy): add GitHub Pages deployment workflow and build script for gh-pages

--- examples/app-crm-minimal/vite.config.ts
+++ examples/app-crm-minimal/vite.config.ts
@@ -4,6 +4,7 @@ import tsconfigPaths from "vite-tsconfig-paths";
 
 // https://vitejs.dev/config/
 export default defineConfig({
+  base: process.env.GITHUB_PAGES ? "/refine/" : "/",
   plugins: [tsconfigPaths({ root: __dirname }), react()],
   build: {
     rollupOptions: {
